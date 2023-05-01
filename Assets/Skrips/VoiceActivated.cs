495b97fb0036fde52be6b0a7828c7586e6aa9735
Insanitori <natoriwolf2p@gmail.com>
2023-05-01 19:14:48 -0400

    Protype

---
+++ Assets/Skrips/VoiceActivated.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceActivated : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RecordingCanvas Rec;
+    private string voice;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
