f1e8174f5a2251f8f42f97e52e5b3fa303730ff7
Insanitori <natoriwolf2p@gmail.com>
2023-05-06 21:14:11 -0400

    lot

--- Assets/Skrips/GameManaging.cs
+++ Assets/Skrips/GameManaging.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+using System;
 
 public class GameManaging : MonoBehaviour
 {
+    public TextMeshProUGUI Timer;
+
+    public bool StartGame;
+    private float timeRemaining;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame = false;
+        timeRemaining = 300;
+        Timer.text = "???";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(StartGame)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("You have Won!");
+                timeRemaining = 0;
+                StartGame = false;
+            }
+
+            Timer.text = timeRemaining.ToString();
+        }
     }
 }
